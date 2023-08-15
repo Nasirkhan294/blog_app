aac4debae8f0bca9446b94001e333c57b3271ded
Nasir <nasirbhatti.nk@gmail.com>
2023-08-15 18:27:09 +0500

    Update schema file with like and comment tables

--- db/migrate/20230815125730_create_comment.rb
+++ db/migrate/20230815125730_create_comment.rb
@@ -2,8 +2,8 @@ class CreateComment < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :content
-      t.references :author, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users }
+      t.references :post, null: false, foreign_key: { to_table: :posts }
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_815_104_158) do
+ActiveRecord::Schema[7.0].define(version: 20_230_815_130_614) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'comments', force: :cascade do |t|
+    t.text 'content'
+    t.bigint 'author_id', null: false
+    t.bigint 'post_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['author_id'], name: 'index_comments_on_author_id'
+    t.index ['post_id'], name: 'index_comments_on_post_id'
+  end
+
+  create_table 'likes', force: :cascade do |t|
+    t.bigint 'author_id', null: false
+    t.bigint 'post_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['author_id'], name: 'index_likes_on_author_id'
+    t.index ['post_id'], name: 'index_likes_on_post_id'
+  end
 
   create_table 'posts', force: :cascade do |t|
     t.string 'title'
@@ -20,6 +39,8 @@ ActiveRecord::Schema[7.0].define(version: 20_230_815_104_158) do
     t.bigint 'author_id'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.integer 'comment_counter'
+    t.integer 'likes_counter'
     t.index ['author_id'], name: 'index_posts_on_author_id'
   end
 
@@ -30,5 +51,9 @@ ActiveRecord::Schema[7.0].define(version: 20_230_815_104_158) do
     t.datetime 'updated_at', null: false
   end
 
+  add_foreign_key 'comments', 'posts'
+  add_foreign_key 'comments', 'users', column: 'author_id'
+  add_foreign_key 'likes', 'posts'
+  add_foreign_key 'likes', 'users', column: 'author_id'
   add_foreign_key 'posts', 'users', column: 'author_id'
 end
