70809a499dd0e89488da8a12752451a9ffae00af
Nasir <nasirbhatti.nk@gmail.com>
2023-08-16 14:18:02 +0500

    Create user model file

--- app/models/comment.rb
+++
@@ -1,4 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :post, class_name: 'post'
-end
--- app/models/post.rb
+++
@@ -1,4 +0,0 @@
-class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :comments, likes, foreign_key: :author_id
-end
