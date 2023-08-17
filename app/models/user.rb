d22ad073d8a6c29902c1899bcf56a35eaa540d07
Nasir <nasirbhatti.nk@gmail.com>
2023-08-17 20:33:02 +0500

    Add specs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
 
   attr_accessor :name, :photo, :bio
 
+  # Validations
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
   end
