86653cbcca7dac8d7d9d3afd8b0860f0a9159f67
Nasir <nasirbhatti.nk@gmail.com>
2023-08-23 18:44:32 +0500

    Fix rubocop linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,15 +4,15 @@ class PostsController < ActionController::Base
     per_page = 10
 
     @posts = Post.includes(:author)
-    .includes(:comments)
-    .where(author: params[:user_id])
-    .order(created_at: :asc)
-    .offset((page.to_i - 1) * per_page)
-    .limit(per_page)
+      .includes(:comments)
+      .where(author: params[:user_id])
+      .order(created_at: :asc)
+      .offset((page.to_i - 1) * per_page)
+      .limit(per_page)
 
     @total_pages = (Post.count.to_f / per_page).ceil
     @author = @posts.first.author
-  end 
+  end
 
   def show
     @post = Post.find(params[:id])
