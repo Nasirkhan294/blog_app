e00ddc42895e876a795b8ec8d1ad3f84feba2907
Nasir <nasirbhatti.nk@gmail.com>
2023-08-24 13:44:29 +0500

    Update routes to get the results

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show, create, new]
   end
+
+  get 'users/:user_id/posts/:post_id/comments/new', to: 'comments#new'
+  post 'users/:user_id/posts/:post_id/comments', to: 'comments#create', as: 'comments'
+  post 'users/:user_id/posts/:post_id/likes', to: 'likes#create', as: 'likes'
 end
