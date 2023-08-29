bfcb01bf54ef3df778c7378fa83f4e5582cbc403
Nasir <nasirbhatti.nk@gmail.com>
2023-08-29 08:03:35 -0700

    Add capybara features specs for users show file

--- spec/features/users_index_spec.rb
+++ spec/features/users_index_spec.rb
@@ -1,27 +1,41 @@
 require 'rails_helper'
 
-RSpec.feature 'User Index', type: :feature do
-  scenario 'visiting the user index page' do
-    User.create(name: 'Tom', photo: 'https://media.gcflearnfree.org/ctassets/topics/246/share_size_large.jpg')
-    User.create(name: 'Lily', photo: 'https://media.gcflearnfree.org/ctassets/topics/246/share_size_large.jpg')
-    visit users_path
+RSpec.describe 'Users#index', type: :system do
+  before(:all) do
+    Comment.delete_all
+    Post.delete_all
+    User.delete_all
+    @tom = User.create(name: 'Tom', photo: 'https://placehold.co/200x133', bio: 'Teacher from Mexico.',
+                       posts_counter: 0)
+    Post.create(author: @tom, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+    @lilly = User.create(name: 'Lilly', photo: 'https://placehold.co/200x133', bio: 'Teacher from Poland.',
+                         posts_counter: 0)
+    @users = User.all
+  end
+
+  it 'I can see the username of all other users.' do
+    visit '/users'
     expect(page).to have_content('Tom')
-    expect(page).to have_content('Lily')
+    expect(page).to have_content('Lilly')
   end
-  scenario 'User index page, checking each users posts' do
-    user1 = User.create(name: 'Tom')
-    User.create(name: 'Lily')
-    Post.create(author: user1, title: 'first post')
-    Post.create(author: user1, title: 'second post')
-    Post.create(author: user1, title: 'third post')
-    visit users_path
-    expect(page).to have_content('3')
-    expect(page).to have_content('0')
+
+  it 'I can see the profile picture for each user.' do
+    visit '/users'
+    @users.each do
+      expect(page).to have_css("img[src='https://placehold.co/200x133']")
+    end
   end
-  scenario 'Redirecting to user show page' do
-    user = User.create(name: 'John')
-    visit users_path
-    click_link 'John'
-    expect(page).to have_current_path(user_path(user))
+
+  it 'I can see the number of posts each user has written.' do
+    visit '/users'
+    @users.each do |user|
+      expect(page).to have_content("Number of posts: #{user.posts_counter}")
+    end
+  end
+
+  it 'When I click on a user, I am redirected to that user\'s show page.' do
+    visit '/users'
+    click_link 'Tom'
+    expect(page).to have_current_path("/users/#{@tom.id}")
   end
 end
--- spec/features/users_show_spec.rb
+++ spec/features/users_show_spec.rb
@@ -1,55 +1,63 @@
 require 'rails_helper'
 
-describe 'User Show Page', type: :feature do
-  scenario "visiting a user's show page" do
-    user = User.create(
-      name: 'Tom',
-      photo: 'https://placehold.co/200x133',
-      bio: 'Teacher from Mexico.'
-    )
+RSpec.describe 'Users#shows', type: :system do
+  before(:all) do
+    Comment.delete_all
+    Post.delete_all
+    User.delete_all
+    @tom = User.create(name: 'Tom', photo: 'https://placehold.co/200x133', bio: 'Teacher from Mexico.',
+                       posts_counter: 0)
+    Post.create(author: @tom, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+    @lilly = User.create(name: 'Lilly', photo: 'https://placehold.co/200x133', bio: 'Teacher from Poland.',
+                         posts_counter: 0)
+    @users = User.all
+  end
 
-    post1 = Post.create(
-      author: user,
-      title: 'hello world',
-      text: 'This is my first post'
-    )
-
-    Post.create(
-      author: user,
-      title: 'Hi rails',
-      text: 'This is another post'
-    )
-
-    Comment.create(
-      post: post1,
-      author: 'Tom',
-      text: 'This is new comment'
-    )
-
-    visit user_path(user)
-
-    expect(page).to have_content('Tom')
+  it 'I can see the user\'s profile picture..' do
+    visit "/users/#{@tom.id}"
     expect(page).to have_css("img[src='https://placehold.co/200x133']")
-    expect(page).to have_content('Number of posts: 2')
+  end
 
-    expect(page).to have_content('hello world')
-    expect(page).to have_content('This is my first post')
-    expect(page).to have_content('Comments: 1,')
-    expect(page).to have_content('Likes: 1')
+  it 'I can see the user\'s username.' do
+    visit "/users/#{@tom.id}"
+    expect(page).to have_content('Tom')
+  end
 
-    expect(page).to have_content('Hi rails')
-    expect(page).to have_content('This is another post')
-    expect(page).to have_content('Comments: 0,')
-    expect(page).to have_content('Likes: 0')
+  it 'I can see the number of posts the user has written.' do
+    visit "/users/#{@tom.id}"
+    expect(page).to have_content('Number of posts: 1')
+  end
 
-    user.recent_three.each do |post|
-      within("a[href='/users/#{user.id}/posts/#{post.id}']") do
-        click_link("Post #{post.id}")
-      end
-      expect(page).to have_current_path(post_path(post))
+  it 'I can see the user\'s bio.' do
+    visit "/users/#{@tom.id}"
+    expect(page).to have_content('Teacher from Mexico.')
+  end
+
+  it 'I can see the user\'s first 3 posts.' do
+    visit "/users/#{@tom.id}"
+    posts = @users[0].recent_three
+    posts.each do |post|
+      expect(page).to have_content(post.title)
     end
+  end
 
-    click_link('See all posts')
-    expect(page).to have_current_path("/users/#{user.id}/posts")
+  it 'I can see a button that lets me view all of a user\'s posts.' do
+    visit "/users/#{@tom.id}"
+    expect(page).to have_content('See all posts')
+  end
+
+  it 'When I click a user\'s post, it redirects me to that post\'s show page.' do
+    visit "/users/#{@tom.id}"
+    posts = @users[0].recent_three
+    unless posts.nil? || posts.empty?
+      click_link posts[0].title
+      expect(page).to have_current_path("/users/#{@tom.id}/posts/#{posts[0].id}")
+    end
+  end
+
+  it 'When I click to see all posts, it redirects me to the user\'s post\'s index page.' do
+    visit "/users/#{@tom.id}"
+    click_link 'See all posts'
+    expect(page).to have_current_path("/users/#{@tom.id}/posts")
   end
 end
