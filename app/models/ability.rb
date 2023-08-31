8c6b1f394f8ffb24dbec540d706b1125d6c7dada
Nasir <nasirbhatti.nk@gmail.com>
2023-08-31 04:07:53 -0700

    Modify ability.rb to correct functionality

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,12 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    # can %i[index show], User
-    # can %i[new create show index], Post
-    # can %i[new create show index], Comment
-    # can %i[update destroy], Post, { author_id: user.id }
-    # can %i[update destroy], Comment, { author_id: user.id }
-    # can :manage, Post if user.is? :admin
-    # can :manage, Comment if user.is? :admin
+    can %i[index show], User
+    can %i[new create show index], Post
+    can %i[new create show index], Comment
+    can %i[update destroy], Post, { author_id: user.id }
+    can %i[update destroy], Comment, { author_id: user.id }
+    can :manage, Post if user.is? :admin
+    can :manage, Comment if user.is? :admin
   end
 end
