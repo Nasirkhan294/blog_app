20845fde63b086daf038d7580c2e979d2804b5bb
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-09-01 16:51:00 +0530

    Update the namespace in routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,13 +13,13 @@ Rails.application.routes.draw do
     end
   end
 
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :users, only: [:index] do
-  #       resources :posts, only: [:index] do
-  #         resources :comments, only: %i[index create]
-  #       end
-  #     end
-  #   end
-  # end
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index] do
+        resources :posts, only: [:index] do
+          resources :comments, only: %i[index create]
+        end
+      end
+    end
+  end
 end
